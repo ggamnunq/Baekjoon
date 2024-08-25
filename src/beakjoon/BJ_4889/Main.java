878b117263c86142a101e3cabbb44bb64af8e69a
DESKTOP-SL0GMOI\user <rlawnsdyd530@gmail.com>
2024-08-26 01:07:03 +0900

    Add BJ_4889

---
+++ src/beakjoon/BJ_4889/Main.java
@@ -0,0 +1,46 @@
+package beakjoon.BJ_4889;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = 0;
+        while (true) {
+
+            String str = br.readLine();
+            if (str.contains("-")) {
+                break;
+                }
+            n++;
+            Stack<Character> stack = new Stack<>();
+            int cnt = 0;
+            for (int i = 0; i < str.length(); i++) {
+
+                char c = str.charAt(i);
+
+                if (c == '{') {
+                    stack.add(c);
+                } else {
+                    if (stack.empty()) {
+                        stack.add('{');
+                        cnt++;
+                    } else {
+                        stack.pop();
+                    }
+                }
+
+            }
+            if (!stack.isEmpty()) {
+                cnt += stack.size() / 2;
+            }
+            System.out.println(n + ". " + cnt);
+        }
+    }
+}
